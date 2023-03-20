adb6d367e0ce95330ac2c22ed255a37a9b7f4b79
Teja Vardhan Ch <104910147+Tej0901@users.noreply.github.com>
2023-03-20 13:03:29 +0530

    Update Exercise_1.sql

--- Exercise_1.sql
+++ Exercise_1.sql
@@ -7,12 +7,12 @@ USE StudentDatabase;
 -- ACTIVITY_2
 CREATE TABLE StudentDetails(
 	ID  VARCHAR(10) NOT NULL,
-	FirstName  VARCHAR(20) NOT NULL,
+	FirstName  VARCHAR(25) NOT NULL,
 	LastName  VARCHAR(25) NOT NULL,
 	Gender TINYTEXT NOT NULL,
 	Email  VARCHAR(50) NOT NULL,
 	YearOfBirth  DATE NOT NULL,
-    PRIMARY KEY(ID)
+    CONSTRAINT PK_Student PRIMARY KEY(ID,Email)
 );
 
 -- ACTIVITY_3
@@ -97,12 +97,22 @@ VALUES("19EC2003",
        "2022"
 );
 
+-- INSERT INTO StudentDetails
+-- VALUES("19EC1002",
+-- 	   "Harshini",
+-- 	   "Datta",
+-- 	   "Female",
+-- 	   "harshini0808@gmail.com",
+-- 	   "2001-06-06",
+-- 	   "2022"
+-- );
+
 -- ACTIVITY_5
 ALTER TABLE StudentDetails
 ADD Branch VARCHAR(20) NOT NULL, ADD Std VARCHAR(10) NOT NULL;
 
 UPDATE StudentDetails
-SET Branch="ECE",Std="UG"
+SET Branch="NA",Std="10th"
 WHERE ID="19EC1021";
 
 UPDATE StudentDetails
@@ -110,7 +120,7 @@ SET Branch="CSE",Std="MCA"
 WHERE ID="19EC1020";
 
 UPDATE StudentDetails
-SET Branch="IT",Std="UG"
+SET Branch="NA",Std="10th"
 WHERE ID="19EC1025";
 
 UPDATE StudentDetails
@@ -138,7 +148,7 @@ WHERE ID="19EC2003";
 INSERT INTO StudentDetails 
 VALUES("19EC1002",
 	   "Harshini",
-	   "Laxman",
+	   "Datta",
 	   "Female",
 	   "harshini0808@gmail.com",
 	   "2001-06-06",
@@ -225,6 +235,136 @@ LIMIT 5;
 select FirstName 
 From StudentDetails
 where FirstName Like "_A%";
+
+select * from StudentDetails;
+
+-- EXERCISE_2
+-- 2
+DROP TABLE Marks;
+CREATE TABLE Marks(
+	ID VARCHAR(10) NOT NULL,
+    subject VARCHAR(20) NOT NULL,
+    mark INT ,
+    FOREIGN KEY (ID) REFERENCES StudentDetails(ID)
+    );
+    
+INSERT INTO Marks VALUES("19EC1021",'maths',99);
+INSERT INTO Marks VALUES("19EC1021",'social',100);
+INSERT INTO Marks VALUES("19EC1020",'maths',89);
+INSERT INTO Marks VALUES("19EC1020",'social',96);
+INSERT INTO Marks VALUES("19EC1018",'maths',87);
+INSERT INTO Marks VALUES("19EC1018",'social',null);
+INSERT INTO Marks VALUES("19EC1025",'maths',90);
+INSERT INTO Marks VALUES("19EC1025",'social',80);
+INSERT INTO Marks VALUES("19EC1029",'maths',77);
+INSERT INTO Marks VALUES("19EC1029",'social',84);
+INSERT INTO Marks VALUES("19EC2003",'maths',97);
+INSERT INTO Marks VALUES("19EC2003",'social',91);
+INSERT INTO Marks VALUES("19EC1056",'maths',null);
+INSERT INTO Marks VALUES("19EC1056",'social',88);
+INSERT INTO Marks VALUES("19EC2005",'maths',93);
+INSERT INTO Marks VALUES("19EC2005",'social',78);
+INSERT INTO Marks VALUES("19EC1002",'maths',null);
+INSERT INTO Marks VALUES("19EC1002",'social',null);
+
+select * from Marks;
+
+-- 3
+SELECT StudentDetails.FirstName,Marks.subject,Marks.mark,StudentDetails.Std
+FROM StudentDetails JOIN Marks
+ON StudentDetails.ID=Marks.ID 
+WHERE Marks.mark>90
+ORDER BY StudentDetails.FirstName;  -- AND StudentDetails.YEAR(YearOfAdmission)='2022';
+
+-- 5
+DELETE FROM StudentDetails
+WHERE StudentDetails.ID='19EC1002';
+-- SELECT * from StudentDetails;
+DELETE FROM Marks
+WHERE Marks.ID='19EC1002';
+-- select * from Marks;
+
+-- 6
+-- DROP TABLE StudentAddress;
+CREATE TABLE StudentAddress(
+	ID VARCHAR(10) NOT NULL,
+    doorNo VARCHAR(10) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    PRIMARY KEY(doorNo),
+    FOREIGN KEY(ID) REFERENCES StudentDetails(ID)
+    );
+
+INSERT INTO StudentAddress VALUES('19EC1021','1-1641','tirupati','AP');
+INSERT INTO StudentAddress VALUES('19EC1020','1-1642','chennai','TN');
+INSERT INTO StudentAddress VALUES('19EC1018','1-1643','pondy','PDY');
+INSERT INTO StudentAddress VALUES('19EC1025','1-1644','tirupati','AP');
+INSERT INTO StudentAddress VALUES('19EC1029','1-1645','gudur','AP');
+INSERT INTO StudentAddress VALUES('19EC1056','1-1646','nellore','AP');
+INSERT INTO StudentAddress VALUES('19EC2005','1-1647','tambaram','TN');
+INSERT INTO StudentAddress VALUES('19EC2003','1-1648','srikalahasti','AP');
+INSERT INTO StudentAddress VALUES('19EC1002','1-1649','potheri','TN');
+
+select * from StudentAddress;
+
+SELECT StudentDetails.FirstName,StudentDetails.Branch 
+FROM StudentDetails JOIN StudentAddress
+ON StudentDetails.ID = StudentAddress.ID
+WHERE StudentAddress.State = 'AP';
+
+-- 7
+SELECT  DISTINCT StudentDetails.ID,StudentDetails.FirstName,StudentDetails.Branch
+FROM StudentDetails LEFT JOIN Marks
+ON  StudentDetails.ID = Marks.ID
+WHERE Marks.mark IS NULL;
+
+
+-- 9_A
+SELECT  DISTINCT StudentDetails.ID,StudentDetails.FirstName,StudentDetails.LastName,SUM(Marks.mark) AS total
+FROM StudentDetails  JOIN Marks
+ON StudentDetails.ID = Marks.ID
+GROUP BY StudentDetails.ID,StudentDetails.FirstName,StudentDetails.LastName
+ORDER BY total DESC
+LIMIT 1;
+
+-- 9_B
+SELECT  DISTINCT StudentDetails.ID,StudentDetails.FirstName,StudentDetails.LastName,SUM(Marks.mark) AS total
+FROM StudentDetails JOIN Marks
+ON StudentDetails.ID = Marks.ID
+GROUP BY StudentDetails.ID,StudentDetails.FirstName,StudentDetails.FirstName,StudentDetails.LastName
+ORDER BY total ASC
+LIMIT 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+
+
+ 
+
+
+
+
+
+
+
 
 
 
